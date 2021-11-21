d5c3e5cdd37c4bf6b61e839d5eee2da5a8f655a7
eudaldbrils <91128741+eudaldbrils@users.noreply.github.com>
2021-11-21 20:31:35 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -12,6 +12,7 @@ require "./lcd"
 #puts JSON.parse(j)
 	
 class Finestra < Gtk::Window
+	#INICIALITZACIO DE LES VARIABLES QUE NECESSITAREM
 	@@button 
 	@@button1
 	@@r
@@ -40,41 +41,52 @@ class Finestra < Gtk::Window
 	
 	
 	def initialize
+		#CONSTRUCTOR
+		
+		#DEFINICIO DE CSS 
 		@@css_provider = Gtk::CssProvider.new
 		@@style_provider = Gtk::StyleProvider::PRIORITY_USER
 		@@css_provider.load(:data=> File.read("project.css"))
 		
+		
+		#CREACIÓ DE LA FINESTRA
 		@@window = Gtk::Window.new("inter")
 		@@window.set_border_width(10)
 		@@window.set_window_position(:CENTER)
+		
+		#BOTO CREUETA DE LA FINESTRA
 		@@window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
 		@@window.style_context.add_provider(@@css_provider,@@style_provider)
 		
 		#@uid=" "
 		
+		#CRACIO DEL OBJECTE LCD QUE ENS PERMETRA MOSTRAR MISSATGES PER PANTALLA
 		@@lcd = Lcd.new 
+		
 		
 		#@@grid1 = Gtk::Grid.new
 		#@@grid1.set_row_homogeneous(true)
 		#@@grid1.set_column_homogeneous(true)
 		
+		#CRACIO DE GRID ON TREBALLAREM AMB LA INGENIERIA DE WIDGETS
 		@@grid2 = Gtk::Grid.new
 		@@grid2.set_row_homogeneous(true)
 		@@grid2.set_column_homogeneous(true)
 		@@grid2.set_row_spacing(10)
 	
 		
-		
+		#CREACIO DEL LABEL DE TEXT DE LA FINESTRA INICIAL
 		#font = Pango::FontDescription.new('20')
 		@@label1 = Gtk::Label.new("Please login with your university card",{:use_underline =>false})
 		@@label1.set_name("labelprincipal")
 		@@label1.style_context.add_provider(@@css_provider,@@style_provider)
-
+		
+		#CREACIO DEL LABEL ON GUARDAREM L'USUARI ACTUAL 
 		@@label2 = Gtk::Label.new("",{:use_underline =>false})
 		@@label2.set_name("labelnomusuari")
 		@@label2.style_context.add_provider(@@css_provider,@@style_provider)
 		
-		
+		#CREACIO DEL LABEL QUE DONA LA BENVINGUDA A L'USUARI
 		@@label3 = Gtk::Label.new("Welcome",{:use_underline =>false})
 		@@label3.set_name("labelwelcome")
 		@@label3.style_context.add_provider(@@css_provider,@@style_provider)
@@ -83,47 +95,73 @@ class Finestra < Gtk::Window
 		#@@label2.override_font(font)
 		#@@label1.override_font(font)
 		
+		#AFEGIM EL LABEL DE LA PANTALLA 1
 		@@grid2.attach(@@label1,0,0,1,1)
 		@@window.add(@@grid2)
 		
+		#CREEM OBJECTE QUE ENS PERMET LLEGIR LA TARGETA NFC 
 		@@r = Rfid.new
 		
+		#BOLEANS QUE ENS PERMETRAN LA GESTIO DEL CANVI DE FINESTRES
 		@@bole=true
 		@@bole2 = true
 		
+		#CREACIO DE LA BARRA DE TEXT QUE ENS PERMETRA ENVIAR QUERRIES
 		@@display = Gtk::TextView.new
 		@@display.set_editable(true)
 		@@display.set_cursor_visible(true)
 		@@display.show
 		
+		#BOTO LOGOUT
 		@@button = Gtk::Button.new(:label =>"Logout")
 		@@button.style_context.add_provider(@@css_provider,@@style_provider)
 
+		#BOTO ENVIAR QUERRY
 		@@button1 = Gtk::Button.new(:label =>"Enviar query")
 		@@button1.style_context.add_provider(@@css_provider,@@style_provider)
 	
 		
 		
-	
+		#BOTO LOGOUT
 		@@button.signal_connect("clicked") do
 			@@button.set_sensitive(false)
+			#BORREM LES TAULES QUE TENIM CREADES
 			remove_grid
+			
+			#PAREM EL TIMER JA QUE NOMES VOLEM QUE CORRI QUAN ESTIGUI A LA FINESTRA 2
 			@@timer.stop
 			@@timer.reset
 			#@@timer.start
+			
+			#PASSEM A LA FINESTRA 1
 			self.finestra1			
 		end
+		
+		#GESTIO DEL BOTO DE ENVIAR QUERRY
 		@@button1.signal_connect("clicked") do
-				remove_grid				
+				#BORREM ELS TAULES EN CAS QUE N'HI HAGIN
+				remove_grid
+				
+				#RESETEJEM LES VARIABLES
 				@@str=""
 				@@database = ""
+			
+				#ARRANQUEM EL TIMER
 				@@timer.stop
 				@@timer.reset
 				@@timer.start
+				
+				#LLEGIM EL QUE ENS TENIM AL DISPLAY
 				@@str=self.coger_mensaje_y_borrar(@@display.buffer)
 				puts @@str
+				
+				#SEPAREM EL MISSATGE PER ? EN CAS QUE N'HI HAGI
 				var = @@str.split("?")
+				
+				#SEMPRE GUARDEM A LA PRIMERA POSICIO DE DATABASE LA POSICIO 0 DE VAR 
 				@@database = var[0]
+			
+				#ENS CONNECTEM AL SERVIDOR
 				connexio_server
 				
 			
@@ -132,21 +170,30 @@ class Finestra < Gtk::Window
 		
 		
 	end
-	
+		#FUNCIO START_TIMER: ARRANQUEM UN COMPTADOR, UN COP PASSI UN TEMPS DETERMINAT HEM DE PASSAR A FINESTRA 1
 		def start_timer
 			puts "timer"
+			
+			#DEFINIM EL QUE HA DE FER EL TIMER UN COP S'ACABI EL CRONOMETRE
 			@@timer = Timer.new(20){
 				puts "hola"
 				#@@timer.reset
+				
+				#BORREM LES TAULES
 				remove_grid
+				
+				#PASSEM A FINESTRA 1
 				finestra1
 			}
-			
+			#SI CRIDEM LA FUNCIO I NO PASSI EL TIMEOUT S'HA DARRENCAR
 			@@timer.start
 
 		end
-	
+		
+		#FUNCIO WICH_DATABASE: FUNCIO QUE ENS PERMET SABER QUIN PARAMETRE VOLEM TRACTAR
 		def which_database
+			
+			#RECORDEM QUE DATABASE ES LA PRIMERA PARAULA DE LA QUERRY QUE ENVIEM ABANS DE ?
 			array = []
 			if @@database == "timetable"
 				array = @@timetables
@@ -156,6 +203,7 @@ class Finestra < Gtk::Window
 				array = @@tasks
 			
 			else  
+				#TRACTAMENT D'ERRORS, SI LA @@DATABASE NO ES CAP DE LES TRES OPCIONS DEFINIDES HA DE MOSTAR MISSATGE ERROR
 				array = ["Error"]
 			
 			end
@@ -163,23 +211,36 @@ class Finestra < Gtk::Window
 			return array	
 	
 		end
+	
+		#FUNCIO REMOVE_GRID: FUNCIO QUE ENS BUIDA EL GRID
 		
 		def remove_grid
 			
 			i = 0
 			j = 0 
+			
+			#MIREM EL LABEL QUE INSERIM A LA FILA 4 I COL 0 SI NO ES NULL L'HEM DE BORRAR
 			label = @@grid2.get_child_at(0,4)
 			if label!=nil
 				@@grid2.remove(label)
 			end 			
 			
+			#ARA PROCEDIM A BORRAR LA TAULA, MENTRES EXISTEIXIN LABELS A LES FILES SEGUIM
 			while @@grid2.get_child_at(0,j+5)!=nil
+				
 				i=0
+				#MENTRES QUDIN LABELS A LA COLUMNA SEGUIM
+				
 				while @@grid2.get_child_at(i,j+5)!=nil
 					label = @@grid2.get_child_at(i,j+5)
+					
+					#BORREM EL CORRESPONENT LABEL
 					@@grid2.remove(label)
+					
+					#ENS MOVEM DE COLUMNA
 					i = i+1
 				end 
+				#ENS MOVEM DE FILA
 				j = j+1
 				
 				
@@ -188,30 +249,46 @@ class Finestra < Gtk::Window
 			
 		
 		end
-	
+		
+		#FUNCIO TAULES: AQUESTA FUNCIO CREE LES TAULES I GESTIONA ERRORS DE QUERRY
 		def taules(resp)
 			
 			array = which_database
+			
+			#SI LA POSICIO 0 ARRAY ES ERROR AFEGIM UN LABEL VERMELL I EL NOM DE L'ERROR QUE ENS PASSA EL SERVIDOR, EL LABEL APAREIX A LA FILA 4 COL0
 			if array[0] == "Error"
 				label = Gtk::Label.new(resp,{:use_underline =>false})
 				label.set_name("labelvermell")
 				label.style_context.add_provider(@@css_provider,@@style_provider)
 				@@grid2.attach(label,0,4,1,1)
 			else
+				#EN CAS QEU NO TINGUEM ERRORS DE QUERRY, AFEGIM UN LABEL QUE A A POS FILA 4 COL 0 DE COLOR VERMELL AMB EL NOM DE LA TAULA QUE ENS MOSTRA
 				label = Gtk::Label.new(@@database,{:use_underline =>false})
 				label.set_name("labelvermell")
 				label.style_context.add_provider(@@css_provider,@@style_provider)
 				@@grid2.attach(label,0,4,1,1)	
+				
+				# RECORREM L'ARRAY I AFEGEIX LA LABELS QUE INDICARAN ELS TITOLS DE LES COLUMNES DE LA TAULA
 				array.each.with_index do|line,i|
 					label = Gtk::Label.new(line,{:use_underline =>false})
 					label.set_name("labeltitols")
 					label.style_context.add_provider(@@css_provider,@@style_provider)
 					@@grid2.attach(label,i,5,1,1)	
 				end
+				
+				#ARA TRACTEM LO QUE ENS ENVIA EL SERVIDOR, PRIMER ANEM MOVENT-NOS PELS SALTS DE L'INEA QUE ENS ENVIEN DES DEL SERVIDOR
 				resp.each_line.with_index do|line,j|
+					
+					#PER A CADA SALT DE LINEA FEM UN JSON.PARSE
 					jsond = JSON.parse(line)
+					
+					#ARA AFEGIREM LES FILES, ARA AL FER EL PARSE TENIM COM UN MAPA 
 					array.each.with_index do|line2,i|
+						
+						#CREEM UN LABEL QUE LI PASSEM PER TEXT LO QUE ENS ENVIA EL SERVIDOR SI LI PASSEM EL TITOL DE LA COLUMNA i
 						label = Gtk::Label.new(jsond[line2],{:use_underline =>false})
+						
+						#PER CANVIAR ELS COLORS DEL LABEL DEPENENT DE LA FILA
 						if j%2==0
 							label.set_name("labelpar")
 						else 
@@ -219,6 +296,8 @@ class Finestra < Gtk::Window
 
 						end
 						label.style_context.add_provider(@@css_provider,@@style_provider)
+						
+						#AFEGIM EL LABEL A LA POSICIO COL i FILA j+6
 						@@grid2.attach(label,i,j+6,1,1)
 					
 					end
@@ -227,26 +306,35 @@ class Finestra < Gtk::Window
 			@@window.show_all
 			puts "hola profe"
 		end 
-	
+
+		#FUNCIO ESPERA_RESPOSTA: EN AQUESTA FUNCIO ESPEREM UNA RESPOSTA DEL SERVIDOR I CRIDEM A LA FUNCIO QUE ENS CREARA LES TAULES
 		def espera_resposta
+			
+			#SI LA QUERRY QUE ENVIEM EL PRIMER PARAMETRE ABANS DE ? ES MARKS HEM DE CONCATENAR: marks&student_id=id_actual
 			if @@database == "marks"
 				@@str<<"&student_id="<<@@uid.upcase
 			end
+			
+			#FEM LA PETICIO AL SERVIDOR
 			resp = HTTP.get("http://192.168.4.46:8000/course_manager.php?"<<@@str).to_s
 			puts"rebo dada"
 			puts resp
+			
+			#DELEGUEM TASQUES A LA FUNCIO TAULES I LI PASSEM PER PARAMETRE LO QUE REBEM DEL SERVIDOR
 			GLib::Idle.add{taules(resp)}
 
 		
 		end
-	
+		
+		#FUNCIO CONNEXIO_SERVER: CREEM UN THREAD I CRIDEM A LA FUNCIO ESPERA_RESPOSTA
 		def connexio_server
 			serv = Thread.new{
 				espera_resposta
 			}
 		
 		end
-	
+		
+		#FUNCIO COGER_MENSAJE_Y_BORRAR: LI PASSEM PER PARAMETRO EL BUFFER DEL DISPLAY, SIMPLEMENT ENS RETORNA UN STRING AMB EL QUE HEM ESCRIT AL DISPLAY 
 		def coger_mensaje_y_borrar(buf)
 			iter1=buf.start_iter
 			iter2=buf.end_iter
@@ -255,14 +343,21 @@ class Finestra < Gtk::Window
 			return missatge
 		end
 		
-		
+		#FUNCIO LLEGEIX: AQUESTA FUNCIO SERVEIX PER LLEGIR UN ID I REBRE UNA RESPOSTA DEL SERVIDOR
 		def llegeix
+				#INICIALITZEM UID I POSEM EN MARXA EL LECTOR
 				@@uid = ""
 				@@uid = @@r.read_uid
 				puts @@uid
+						
+				#UN COP HEM LLEGIT PASSEM A FER UNA PETICIO AL SERVIDOR
 				body = HTTP.get("http://192.168.4.46:8000/course_manager.php?students?uid="<<@@uid).to_s
 				puts body
+				
+				#SI L'UID QUE HEM LLEGIT NO EXISTEIX A LA BASE DE DADES HEM DE FER LA SEVA GESTIO
 				if body== ""
+					
+					#SOBREESCRIVIM BODY AMB UN STRING BUIT EL QUAL PUGUEM FER UN JSON.PARSE
 					body ='{"uid":"","name":""}'
 
 				end
@@ -270,10 +365,12 @@ class Finestra < Gtk::Window
 				puts var
 				
 
-				#Punt clau del codi, delegar la tasca a canvipantalles
+				#DELEGUEM LA TASCA AL CANVIPANTALLES 
 				GLib::Idle.add{canvipantalles(var["uid"],var["name"])}
 		end
-			def fil
+	
+		#FUNCIO FIL: THREAD QUE CRIDA A LA FUNCIO LLEGEIX 
+		def fil
 			t = Thread.new{
 				llegeix
 				puts "mato"
@@ -281,12 +378,16 @@ class Finestra < Gtk::Window
 			}
 		end
 		
+		#FUNCIO CANVIPANTALLES: FUNCIO ENCARREGADA DEL CANVI DE PANTALLES, LI PASSEM PER PARAMETRES EL UID QUE ENS RETORNA EL SERVIOR I LE NOM DE L'USUARI
 		def canvipantalles(str_uid,name)
 			puts "canvipantalles"
+			
+			#SI EL UID QUE HEM LLEGIT CONCORDA AMB EL QUE ENS PASSA EL SERVIDOR PASSEM A FINESTRA2 
 			if @@uid.upcase == str_uid#falta completard
 				puts "finestra2"
 				self.finestra2(name)
-			else
+			
+			else #SI EL UID QUE HEM LLEGIT NO CONCORDA AMB EL QUE HEM PASSAT S'HA D'ESCRIURE UN MISSATGE D'ERROR I QUEDAR-SE A FINESTRA1
 				puts "uknown"
 				@@lcd.escriure("    Unknown user ")
 				finestra1(true)
@@ -296,9 +397,12 @@ class Finestra < Gtk::Window
 			puts "surto"
 			
 		end
-			def finestra1(bole_origin=false)
-			#S'hauria d'arreglar(CODI MOLT BRUT)
-			if @@bole==false
+		
+		#FUNCIO FINESTRA1, LI PASSEM PER PARAMETRO UN BOOLEA QUE EN PRINVIPI SERA FALS PERO QUE EL PODEM FER VARIAR
+		def finestra1(bole_origin=false)
+			
+			#SI ES LA ES LA PRIMERA VEGADA QUE ENTREM A FINESTRA 1 NO CAL FER CAP REMOVE
+		-	if @@bole==false
 				#@@grid2.remove(@@label3)
 				@@grid2.remove(@@button)
 				@@grid2.remove(@@button1)
@@ -307,10 +411,13 @@ class Finestra < Gtk::Window
 				#@@window.remove(@@grid2)
 				
 			end
+			
+			#SI EL BOOLEA QUE HEM PASSAT ES TRUE LLAVORS VOL DIR QUE HI HA HAGUT UN ERROR AL REGISTRE I QUE L'USUARI NO ESTA A LA BASE DE DADES
 			if bole_origin == true
 				@@lcd.escriure("Unknown user\nPlease login with\n your univerity card")
 				@@label1.text = "                   Unknown user:\nplease login with your univerity card"
 			else
+				#SI EL BOOLEA ES FALSE VOL DI QUE NO HI HA CAP PROBLEMA AMB EL REGISTRE
 				@@lcd.escriure("Please login with\n your univerity card")
 				@@label1.text = "Please login with your univerity card"
 			
